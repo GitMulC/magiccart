5b5cab640d03bbf3049963fbef8db7037c2d8602
GitMulC <cmulvo@gmail.com>
2022-07-27 19:48:55 +0000

    Add new project & delete prev as error in running manage.py

--- magic_cart/asgi.py
+++ magic_cart/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for magiccart project.
+ASGI config for magic_cart project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'magiccart.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'magic_cart.settings')
 
 application = get_asgi_application()
--- magic_cart/wsgi.py
+++ magic_cart/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for magiccart project.
+WSGI config for magic_cart project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'magiccart.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'magic_cart.settings')
 
 application = get_wsgi_application()
