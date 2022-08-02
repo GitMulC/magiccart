93e3a5d4bed4cf45760b7d4795268d02e2678d85
GitMulC <cmulvo@gmail.com>
2022-08-02 17:55:19 +0000

    Create product model in models.py

--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    color = CloudinaryField('mana_symbol')
+    oracle_text = models.CharField(max_length=254)
+    flavor_text = models.CharField(max_length=254)
+    set = models.CharField(max_length=254)
+    rarity = models.CharField(max_length=254)
+    cmc = models.DecimalField(max_digits=3, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    version = models.CharField(max_length=254)
