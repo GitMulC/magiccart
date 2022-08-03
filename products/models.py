3f7975561a2eca799fa6b708dc5f44cb8cf6848b
GitMulC <cmulvo@gmail.com>
2022-08-03 17:35:23 +0000

    Edit cmc field in card model

--- products/models.py
+++ products/models.py
@@ -9,7 +9,7 @@ class Card(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     set = models.CharField(max_length=254)
     rarity = models.CharField(max_length=254)
-    cmc = models.DecimalField(max_digits=3, decimal_places=2, blank=True)
+    cmc = models.DecimalField(max_digits=2, decimal_places=1, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     version = models.CharField(max_length=254, blank=True)
 
