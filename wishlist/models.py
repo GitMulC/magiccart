bf39603f50a7b8f1d2cf5a5226479c31138f10fb
GitMulC <cmulvo@gmail.com>
2022-08-03 18:45:17 +0000

    Add wishlist model & add to settings.py

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Wishlist(models.Model):
+    card = models.ForeignKey('Card', on_delete=models.SET_NULL)
+    date_added = models.DateField(auto_now=True)
+    # price = models.ForeignKey('Card', on_delete=models.SET_NULL)
+    user = models.ForeignKey()
