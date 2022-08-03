bd85ff4e74cd812f939217842ccef506dfdc1070
GitMulC <cmulvo@gmail.com>
2022-08-03 16:10:11 +0000

    Edit products model and change to card models

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Card
 
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Card)
--- products/models.py
+++ products/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 
-class Product(models.Model):
+class Card(models.Model):
     name = models.CharField(max_length=30)
     color = CloudinaryField('mana_symbol')
-    oracle_text = models.CharField(max_length=254)
-    flavor_text = models.CharField(max_length=254)
+    oracle_text = models.CharField(max_length=254, blank=True)
+    flavor_text = models.CharField(max_length=254, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     set = models.CharField(max_length=254)
     rarity = models.CharField(max_length=254)
-    cmc = models.DecimalField(max_digits=3, decimal_places=2)
+    cmc = models.DecimalField(max_digits=3, decimal_places=2, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    version = models.CharField(max_length=254)
+    version = models.CharField(max_length=254, blank=True)
+
+    def __str__(self):
+        return self.name
