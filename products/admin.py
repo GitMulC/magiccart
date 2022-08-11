a6b70d9f31fa2b2dad653ba9f6a085a6c0336290
GitMulC <cmulvo@gmail.com>
2022-08-11 19:32:19 +0000

    Add catergory model, edit card model

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Card
+from .models import Card, Catergory
 
 # Register your models here.
 admin.site.register(Card)
+admin.site.register(Catergory)
+
--- products/models.py
+++ products/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 
+class Catergory(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
 class Card(models.Model):
     name = models.CharField(max_length=30)
     color = CloudinaryField('mana_symbol')
@@ -11,8 +21,8 @@ class Card(models.Model):
     rarity = models.CharField(max_length=254)
     cmc = models.DecimalField(max_digits=2, decimal_places=1, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    card_img = CloudinaryField('card')
-    card_img_url =models.URLField(max_length=1024, null=True, blank=True)
+    card_img = CloudinaryField('card', null=True, blank=True)
+    card_img_url = models.URLField(max_length=1024, blank=True)
 
     def __str__(self):
         return self.name
