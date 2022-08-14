3a55bdb2d219636496aabd922e069ce91c1bb445
GitMulC <cmulvo@gmail.com>
2022-08-14 10:03:31 +0000

    Add product view for site

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Card
 
 # Create your views here.
+def all_cards(request):
+    """A view to show all card products & incl searching and sorting"""
+
+    cards = Card.objects.all()
+
+    context = {
+        'products': cards,
+    }
+
+    return render(request, 'cards/cards.html', context)
