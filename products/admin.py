2ddb81836154c82cb336d74889d272421ac2749e
GitMulC <cmulvo@gmail.com>
2022-08-14 09:36:15 +0000

    Add admin customization

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,23 @@ from django.contrib import admin
 from .models import Card, Type
 
 # Register your models here.
-admin.site.register(Card)
-admin.site.register(Type)
+class CardAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'color',
+        'type',
+        'rarity',
+        'price',
+    )
+
+    ordering = ('name',)
+
+class TypeAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Card, CardAdmin)
+admin.site.register(Type, TypeAdmin)
