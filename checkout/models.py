920593ad6f4edcdda676261e10b136b290c8f8eb
GitMulC <cmulvo@gmail.com>
2022-08-16 20:52:17 +0000

    Add order number generation to order & orderlineitem model

--- checkout/models.py
+++ checkout/models.py
@@ -23,6 +23,33 @@ class Order(models.Model):
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
 
+    def _generate_order_number(self):
+        """ Generate a random order number using UUID """
+
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+        """Update grand total each time a line item is added, accounting for delivery costs."""
+
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+        else:
+            self.delivery_cost = 0
+        self.grand_total = self.order_total + self.delivery_cost
+        self.save()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        if it hasn't been set already.
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
 
 
 
@@ -32,3 +59,10 @@ class OrderLineItem(models.Model):
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
+    def save(self, *args, **kwargs):
+        """ Override the original save method to set the lineitem total and update the order total. """
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order
