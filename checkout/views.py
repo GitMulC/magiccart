7b5285352066f7b80638e8334769f982fca921cd
GitMulC <cmulvo@gmail.com>
2022-08-16 22:16:48 +0000

    Add checkout view to checkout template, correct spelling errors

--- checkout/views.py
+++ checkout/views.py
@@ -8,12 +8,12 @@ def checkout(request):
     bag = request.session.get('bag', {})
     if not bag:
         message.error(request, "There's nothing in your bag at the moment")
-        retunr redirect(request('products'))
+        return redirect(request('products'))
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_from,
+        'order_form': order_form,
     }
 
     return render(request, template, context)
