956563a3ab0277fce2fc119c0b1c582f9252fe13
GitMulC <cmulvo@gmail.com>
2022-08-16 21:17:59 +0000

    Add forms, admin, and signals to checkout app for backend completion of form

--- checkout/models.py
+++ checkout/models.py
@@ -18,6 +18,7 @@ class Order(models.Model):
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
---
+++ checkout/signals.py
@@ -0,0 +1,16 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from .models import OrderLineItem
+
+@receiver(post_save, sender=OrderInLineItem)
+def update_on_save(sender, instance, created, **kwargs):
+    """ Update an order total on inlineitem update/create """
+
+    instance.order.update_total()
+
+@receiver(post_delete, sender=OrderInLineItem)
+def update_on_save(sender, instance, created, **kwargs):
+    """ Update an order total on inlineitem delete """
+
+    instance.order.update_total()
