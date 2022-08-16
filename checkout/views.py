0256361a7b4c3cef9267d30c59f2186d49c94dcb
GitMulC <cmulvo@gmail.com>
2022-08-16 21:23:00 +0000

    Add views & urls for checkout app

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+
+from .forms import OrderForm
 
 # Create your views here.
+
+def checkout(request):
+    bag = request.session.get('bag', {})
+    if not bag:
+        message.error(request, "There's nothing in your bag at the moment")
+        retunr redirect(request('products'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_from,
+    }
+
+    return render(request, template, context)
