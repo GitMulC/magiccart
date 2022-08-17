ab23de4be8849e0b23e6e9e3018b73d225f85470
GitMulC <cmulvo@gmail.com>
2022-08-17 16:48:02 +0000

    Add stripe links & elements to templates and views

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51LXp45G7PugiIHvNxfZUr49w3sNZ3c1h92iDQVh5E08omMd8QRBb6fYJ2mvnZskW5vF8ewHQolM4Zrylz39pODGF00wobI9XFl',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
