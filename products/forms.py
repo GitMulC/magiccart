f80ef90bd4ddd3d43b26aa420ec23c70b11896a1
GitMulC <cmulvo@gmail.com>
2022-08-22 16:21:58 +0000

    Add forms.py file to allow store owner to upload cards

---
+++ products/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Card, Type
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Card
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        types = Type.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name() for t in types)]
+
+        self.fields['type'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attr['class'] = 'border-black rounded-0'
