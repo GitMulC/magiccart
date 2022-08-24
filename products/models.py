2e8cdd98efe5d5564cf6bb4690f11171946634af
GitMulC <cmulvo@gmail.com>
2022-08-24 20:18:44 +0000

    Reset postgres & local database to solve migration issues

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
 class Type(models.Model):
     name = models.CharField(max_length=254)
@@ -12,18 +13,18 @@ class Type(models.Model):
         return self.friendly_name
 
 class Card(models.Model):
-    type = models.ForeignKey('Type', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=30)
-    color = CloudinaryField('mana_symbol')
+    color = models.CharField(max_length=254)
+    cmc = models.DecimalField(max_digits=2, decimal_places=1, null=True, blank=True)
+    card_img = CloudinaryField('card', null=True, blank=True)
+    card_img_url = models.URLField(max_length=1024, blank=True)
+    type = models.ForeignKey('Type', null=True, blank=True, on_delete=models.SET_NULL)
+    set = models.TextField(blank=True)
+    rarity = models.TextField(blank=True)
     oracle_text = models.TextField(blank=True)
     flavor_text = models.TextField(blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    set = models.TextField(blank=True)
-    rarity = models.TextField(blank=True)
-    cmc = models.DecimalField(max_digits=2, decimal_places=1, null=True, blank=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    card_img = CloudinaryField('card', null=True, blank=True)
-    card_img_url = models.URLField(max_length=1024, blank=True)
+    user_favorite = models.ManyToManyField(User, related_name='wishlist_item', blank=True)
 
     def __str__(self):
         return self.name
