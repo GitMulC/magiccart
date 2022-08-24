94c4e652ae27ae542c858290078b0b9f08348f77
GitMulC <cmulvo@gmail.com>
2022-08-24 18:20:24 +0000

    Add basic wishlist view to wishlist app. Add app to urls & add urls.py file, also add link to cards template

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from products.models import Card
 
-# Create your views here.
+@login_required
+def display_wishlist(request):
+    """ A view for showing a user all their wishlist items """
+    wishlist_item = Card.objects.filter(user_favorite=request.user)
+
+    context = {
+        'wishlist_item': wishlist_item,
+    }
+
+    return render(request, 'wishlist/wishlist.html', context)
+    
