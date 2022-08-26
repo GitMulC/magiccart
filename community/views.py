3ade565f45b5ae0edebcbc814d2bd14270294412
GitMulC <cmulvo@gmail.com>
2022-08-26 23:24:32 +0000

    Add urls, views & template to communities app, and add url to project

--- community/views.py
+++ community/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Community
 
 # Create your views here.
+
+def share_communities(request):
+    """ A view to show magic the gathering sites they can use """
+    communities = Community.objects.all()
+
+    template = 'communities/communities.html'
+
+    context = {
+        'communities': communities,
+    }
+
+    return render(request, template, context)
--- magic_cart/urls.py
+++ magic_cart/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('wishlist/', include('wishlist.urls')),
     path('maillist/', include('maillist.urls')),
+    path('community/', include('community.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
