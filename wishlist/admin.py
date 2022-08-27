be4e63c803d13f1208723deb7ad81941cd731e2b
GitMulC <cmulvo@gmail.com>
2022-08-27 15:21:23 +0000

    Render out wishlist in wishlist template from profile

--- wishlist/admin.py
+++ wishlist/admin.py
@@ -1,4 +1,14 @@
-from django.contrib import admin
+# from django.contrib import admin
+# from .models import Wishlist
 
-# Register your models here.
-# admin.site.register(Wishlist)
+# # Register your models here.
+
+# class WishlistAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'date_added',
+#         'price',
+#         'card',
+#         'user',
+#     )
+
+# admin.site.register(Wishlist, WishlistAdmin)
--- wishlist/models.py
+++ wishlist/models.py
@@ -1,10 +1,14 @@
-from django.db import models
+# from django.db import models
 # from django.contrib.auth.models import User
 # from cloudinary.models import CloudinaryField
+# from products.models import Card
 
-# Create your models here.
+# # Create your models here.
 # class Wishlist(models.Model):
-#     card = models.ForeignKey('Card', on_delete=models.SET_NULL)
+#     card = models.ForeignKey('products.Card', on_delete=models.SET_NULL)
 #     date_added = models.DateField(auto_now=True)
-#     # price = models.ForeignKey('Card', on_delete=models.SET_NULL)
-#     user = models.ForeignKey()
+#     price = models.ForeignKey('products.Card', on_delete=models.SET_NULL)
+#     user = models.ForeignKey('profiles.User', on_delete=models.SET_NULL)
+
+#     def __str__(self):
+#         return self.card
