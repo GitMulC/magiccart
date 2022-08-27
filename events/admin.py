cae5d31a5d31fd7ef8f8faaa8d5a33a3a22a9237
GitMulC <cmulvo@gmail.com>
2022-08-27 14:02:34 +0000

    Add event model & model admin for events app

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Event
 
 # Register your models here.
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('date', 'name', 'location',)
+
+    ordering = ('date',)
+
+
+admin.site.register(Event, EventAdmin)
--- events/models.py
+++ events/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Event(models.Model):
+    name = models.CharField(max_length=254)
+    location = models.CharField(max_length=254)
+    date = models.DateTimeField()
+    contact_info = models.CharField(max_length=254, blank=True)
+
+    def __str__(self):
+        return self.name
