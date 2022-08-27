f0cb17372a5cbdd39cef93140239a967cb7c9e56
GitMulC <cmulvo@gmail.com>
2022-08-27 15:24:49 +0000

    Add 404 handler, url and template for 404 errors

--- magic_cart/urls.py
+++ magic_cart/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,3 +31,5 @@ urlpatterns = [
     path('maillist/', include('maillist.urls')),
     path('community/', include('community.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'boutique_ado.views.handler404'
