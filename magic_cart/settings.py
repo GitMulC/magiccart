2a9b0366be53cf3cebb17e4c4e8d579895fb0f16
GitMulC <cmulvo@gmail.com>
2022-08-28 14:45:53 +0000

    Add debug logic to settings.py

--- magic_cart/settings.py
+++ magic_cart/settings.py
@@ -27,7 +27,7 @@ ALLAUTH_DIR = os.path.join(BASE_DIR, 'templates', 'allauth')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['magic-cart.herokuapp.com', 'localhost']
 
