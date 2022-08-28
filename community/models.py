7dbaf097ae04e04b39bd777e854e17c84dbac83c
GitMulC <cmulvo@gmail.com>
2022-08-28 14:31:55 +0000

    Change logo img field to cloudinary field

--- community/models.py
+++ community/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
@@ -7,7 +8,7 @@ class Community(models.Model):
     name = models.CharField(max_length=254, unique=True)
     description = models.TextField()
     url = models.URLField(max_length=1024)
-    logo = models.ImageField()
+    logo = CloudinaryField('logo', null=True, blank=True)
 
     def __str__(self):
         return self.name
